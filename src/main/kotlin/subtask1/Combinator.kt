8f2c7bf0c3569067adbfc0d4fe5e5750d74cc890
“Dmitry <yakub.dm@mail.ru>
2021-07-14 22:30:57 +0300

    task complit

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,25 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val m = array[0] //number of posters
+        val n = array[1] //available colors
+        var x = 0
+        var combinationsNumber = 1
+
+        fun factorial(number: Int): Long{
+            var numberFactorial = 1L
+            for (i in 1..number) numberFactorial*=i
+            return numberFactorial
+        }
+
+        while (combinationsNumber < m && combinationsNumber != 0){
+            x++
+            combinationsNumber = (factorial(n) / (factorial(n - x) * factorial(x))).toInt()
+            println("cn "+ combinationsNumber)
+        }
+
+        if (combinationsNumber == m) return x
+        else return null
     }
 }
